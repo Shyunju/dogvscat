00326bb48332e204cc34d6fcb3bee76d3dd03153
Shyunju <hyunju9908@naver.com>
2023-04-22 15:35:04 +0900

    dog
    mousePos moving limit

--- Assets/script/dog.cs
+++ Assets/script/dog.cs
@@ -13,6 +13,16 @@ public class dog : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float x = mousePos.x;
+        if (x > 8.5f)
+        {
+            x = 8.5f;
+        }
+        if (x < -8.5f)
+        {
+            x = -8.5f;
+        }
+        transform.position = new Vector3(x, transform.position.y, 0);
     }
 }
