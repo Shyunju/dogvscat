ae4cc13ebd023c8438874cd630fbcb70ad1ccc56
Shyunju <hyunju9908@naver.com>
2023-04-27 10:11:52 +0900

    cat
    cat instatiate

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -6,10 +6,12 @@ public class GameManager : MonoBehaviour
 {
     public GameObject dog;
     public GameObject food;
+    public GameObject normalCat;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("makeFood", 0.0f, 0.2f);
+        InvokeRepeating("makeCat", 0.0f, 1f);
     }
 
     // Update is called once per frame
@@ -23,5 +25,9 @@ public class GameManager : MonoBehaviour
         float x = dog.transform.position.x;
         float y = dog.transform.position.y + 0.2f;
         Instantiate(food, new Vector3(x, y, 0), Quaternion.identity);
+    }
+    void makeCat()
+    {
+        Instantiate(normalCat);
     }
 }
